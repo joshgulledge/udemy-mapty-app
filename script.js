f932b36fa5684aa65eb101a1e51f38e0d6a8f3eb
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-03 12:55:40 -0600

    got current location

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// the iff is to check against older browersers
+if (navigator.geolocation) {
+  // this is a browser API
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //   const latitude = position.coords.latitude
+      const { latitude } = position.coords;
+      //   ^destructoring creates a variable out of the object
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},`);
+    },
+    function () {
+      alert('Could not get current position. ');
+    }
+  );
+}
